664efd1dfd79e2308958a59786beb82d631242f4
Clément Chaine <clement.chaine25@gmail.com>
2025-10-26 11:40:27 +0100

    Add helloworld uart test script

--- tests/uart-test.py
+++ tests/uart-test.py
@@ -21,15 +21,63 @@
 # You should have received a copy of the GNU General Public License
 # along with ECAP5-BCARRIER-XLITE.  If not, see <http://www.gnu.org/licenses/>.
 
-#import pyftdi.serialext
+import pyftdi.serialext
+import pyftdi.ftdi
+import threading
 import argparse
 import textwrap
 import sys
+import random
+import string
+import time
 
-def test_helloworld(write_port : str, read_port : str) -> int:
+HELLOWORLD_BAUDRATE = 115200
+
+def list_devices() -> int:
+    pyftdi.ftdi.Ftdi.show_devices()
     return 0
 
-def test_bandwidth(write_port : str, read_port : str) -> int:
+def receive_thread(port : str, baudrate : int, num_bytes : int, read_data : list[bytes]) -> None:
+    reader = pyftdi.serialext.serial_for_url(port, baudrate=baudrate, timeout=0.1)
+
+    done = False
+    while not done:
+        read_data[0] = reader.read(num_bytes)
+        if(len(read_data[0]) > 0):
+            done = True
+
+    reader.flushInput()
+    reader.flushOutput()
+    reader.close()
+
+    read_data[0] = read_data[0].decode("utf-8")
+    return
+
+def test_helloworld(write_device: str, read_device: str) -> int:
+    data = "Hello World!"
+
+    # Read data will be written in this mutable table
+    read_data = [[]]
+    # Start the reading thread first
+    rt = threading.Thread(target=receive_thread, args=(read_device, HELLOWORLD_BAUDRATE, len(data), read_data))
+    rt.start()
+    
+    # Write the data
+    writer = pyftdi.serialext.serial_for_url(write_device, baudrate=HELLOWORLD_BAUDRATE)
+    writer.write(data)
+
+    # Wait for the data to be read
+    rt.join()
+    writer.close()
+
+    read_data = read_data[0]
+
+    print("Written: {} -> {}".format(write_device, data))
+    print("Read:    {} -> {}".format(read_device, read_data))
+
+    return data != read_data
+
+def test_bandwidth(write_device : str, read_device: str) -> int:
     return 0
 
 if __name__ == "__main__":
@@ -42,16 +90,24 @@ if __name__ == "__main__":
                 back the data on the USB link.
             '''))
 
-    parser.add_argument('mode', choices=['helloworld', 'bandwidth'])
-    parser.add_argument('-i', '--injecting-port', required=True)
-    parser.add_argument('-u', '--usb-port', required=True)
+    parser.add_argument('mode', choices=['list', 'helloworld', 'bandwidth'])
+    parser.add_argument('-d1', '--device1')
+    parser.add_argument('-d2', '--device2')
 
     args = parser.parse_args()
+
+    if args.mode in ['helloworld', 'bandwidth'] and ((args.device1 is None) or (args.device2 is None)):
+        parser.error("--device1 and --device2 are required")
+
+    if args.mode == 'list':
+        err = list_devices()
+        sys.exit(err)
     if args.mode == 'helloworld':
-        err = test_helloworld(args.injecting_port, args.usb_port)
+        err  = test_helloworld(args.device1, args.device2)
+        err |= test_helloworld(args.device2, args.device1)
         sys.exit(err)
     elif args.mode == 'bandwidth':
-        err = test_bandwidth(args.injecting_port, args.usb_port)
+        err = test_bandwidth(args.device1, args.device2)
         sys.exit(err)
     else:
         print("Unimplemented mode : {}".format(args.mode))
